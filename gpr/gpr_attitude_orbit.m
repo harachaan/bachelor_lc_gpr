ec561cbe5faa48e02debb82abb268b6ee77c6eb2
Your Name <you@example.com>
2022-11-07 12:35:00 +0900

    can output data to csv file

---
+++ gpr/gpr_attitude_orbit.m
@@ -0,0 +1,37 @@
+% 姿勢，軌道を状態変数(変数７個？)に持つGPR
+clc
+clear
+close all
+
+% kernel parameters
+tau = log(1);
+sigma = log(1);
+eta = log(1);
+params = [tau sigma eta];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
