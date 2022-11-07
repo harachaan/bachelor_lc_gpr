358faa00d0408a8e9fa996109702c9c56dab13a2
harachaan <hr1021ry@icloud.com>
2022-11-07 17:54:23 +0900

    create 10 samples for trainingdata using rand()

--- light_curves_model/attitude_orbit_data/simple_plate/make_samples.m
+++ light_curves_model/attitude_orbit_data/simple_plate/make_samples.m
@@ -3,14 +3,14 @@ function samples = make_samples(filename, numheader)
 
     data = readmatrix(filename, "NumHeaderLines", numheader);
     % 区間(0, 1)の一様分布からの乱数をもとの平板データに足す
-    x = rand(length(data), 7, 10);
+    x = 1e-3 * (0.5 - rand(length(data), 7, 10));
     samples = zeros(length(data), 7, length(x(1,1,:)));
     for i = 1:1:length(x(1,1,:))
-        samples(:,:,i) = samples(:,:,i) + x(:,:,i);
-        data = array2table(samples(:,:,i));
-        data.Properties.VariableNames(1:7) = {'q0', 'q1', 'q2', 'q3', 'x_cowell', 'y_cowell', 'z_cowell'};
+        samples(:,:,i) = data + x(:,:,i);
+        sample = array2table(samples(:,:,i));
+        sample.Properties.VariableNames(1:7) = {'q0', 'q1', 'q2', 'q3', 'x_cowell', 'y_cowell', 'z_cowell'};
         savename = strcat('simple_plate_', num2str(i), '.csv');
-        writetable(data, savename);
+        writetable(sample, savename);
     end
 
 end
