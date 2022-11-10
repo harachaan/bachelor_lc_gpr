8c42d7c6027b10a408ee4b5d85db4b64fa49a964
Your Name <you@example.com>
2022-11-10 16:54:27 +0900

    add yoshimulibrary for creating training data

---
+++ yoshimulibrary/yoshimuLibrary-main/utility/cls.m
@@ -0,0 +1,17 @@
+function cls
+% ----------------------------------------------------------------------
+%   close all figure windows
+%    20210825  y.yoshimura
+%    Inputs:
+%   Outputs:
+%   related function files:
+%   note:
+%   cf:
+%   revisions;
+%   
+%   (c) 2021 yasuhiro yoshimura
+%----------------------------------------------------------------------
+
+close all
+
+end
